3c686acee812061300f4044e653f384983cc7067
Reece Lucas <reecelucas@sky.com>
2018-04-21 00:10:18 +0100

    Global settings updated. Spacer utility now tolerates breakpoints

--- src/components/utility/Spacer/Spacer.tsx
+++ src/components/utility/Spacer/Spacer.tsx
@@ -3,20 +3,46 @@ import { ClassMap } from '../../../types';
 
 const styles = require('./Spacer.module.scss');
 
+type Size = 'tiny' | 'small' | 'large' | 'huge';
+type SizeObject = {
+    value?: Size;
+    breakpoint: 'medium' | 'large';
+};
+
 interface Props {
     children?: any;
-    size?: 'tiny' | 'small' | 'large' | 'huge';
+    size?: Size | SizeObject[];
 }
 
 const classMap: ClassMap = {
-    tiny: styles['spacerTiny'],
-    small: styles['spacerSmall'],
-    large: styles['spacerLarge'],
-    huge: styles['spacerHuge']
+    base: 'spacer',
+    tiny: 'spacerTiny',
+    small: 'spacerSmall',
+    large: 'spacerLarge',
+    huge: 'spacerHuge'
+};
+
+const breakpointMap = {
+    medium: '@md',
+    large: '@lrg'
+};
+
+const constructClassList = (size: Props['size']): string => {
+    if (!size) return styles.spacer;
+
+    if (typeof size === 'string') {
+        return styles[classMap[size] || classMap.base];
+    }
+
+    return size
+        .map(({ value, breakpoint }) =>
+            styles[`${classMap[value] || classMap.base}${breakpointMap[breakpoint]}`]
+        )
+        .join(' ');
 };
 
 const Spacer = ({ children, size }: Props) => (
-    <div className={classMap[size] || styles.spacer}>{children}</div>
+    <div className={constructClassList(size)}>{children}</div>
 );
 
 export default Spacer;
